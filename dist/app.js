7ea961500557bb6c9c69a5d419749e9832a3b033
charbel abi khalil <charbelak31@gmail.com>
2024-05-03 00:36:27 +0300

    2

--- dist/app.js
+++ dist/app.js
@@ -3,18 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const app = (0, express_1.default)();
 const express_1 = __importDefault(require("express"));
-// import userRoutes from "./src/user/user.routes";
 const database_1 = require("./config/database");
 const token_routes_1 = __importDefault(require("./token/token.routes"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const body_parser_1 = __importDefault(require("body-parser"));
-dotenv_1.default.config();
-const app = (0, express_1.default)();
-const PORT = process.env.PORT;
-app.use(express_1.default.json());
+//app.use(express.json());
 app.use(body_parser_1.default.json());
+dotenv_1.default.config();
 (0, database_1.connect)();
+const PORT = process.env.PORT;
 app.use((req, res, next) => {
     console.log("fet 3al IDP");
     res.status(200).json("re7et 3al IDP w rje3et ");
